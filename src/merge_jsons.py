89d2cebc0e066237adcb5953268b3bca5fee492c
Jakub Slowinski <32519034+slow-J@users.noreply.github.com>
2023-08-18 12:46:40 +0100

    Add description to -h cli option

--- src/merge_jsons.py
+++ src/merge_jsons.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python3
 
 """
-This will merge 2 or more results.json files together to present in one "serve" report.
+This tool will merge 2 or more results.json files together to present in one "serve" report.
 
-This tool will replace keys in the merge, so you NEED unique names for EACH engines in EACH json.
+The keys will be merged, so you NEED unique names for EACH engines in EACH json.
 e.g. in json1 you could have the engine called "graviton2-tantivy-0.20" and in json2, "graviton3-tantivy-0.20".
-Same for other engines.
-
-Sample usage:
-    python3 src/merge_jsons.py --input_jsons foo.json bar.json more.json --output_json ./results2.json
-
 
 Dependencies:
     * mergedeep
 
-pip3 install mergedeep to download this.
+Sample usage:
+    python3 src/merge_jsons.py --input_jsons foo.json bar.json more.json --output_json ./results2.json
 """
 import argparse
 import json
 
 from mergedeep import merge
 
+# Installing dependencies:
+# pip3 install mergedeep
+
+# sed commands for easy changes to the results.json keys
+# sed -i -e "s/"tantivy-0.20"/"graviton-tantivy-0.20"/g" ./results_graviton.json
+# sed -i -e "s/"lucene-9.7.0"/"graviton-lucene-9.7.0"/g" ./results_graviton.json
+
 
 def parse_args():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
 
     parser.add_argument(
         "-js",
